ec34425ccf776cb2627725e4528e590cc690d548
Natalie Ike <nataliesenger@Natalies-MBP.fios-router.home>
2017-08-25 17:07:20 -0700

    initialize and setups

---
+++ liri.js
@@ -0,0 +1,7 @@
+//Link to required Node packages
+var inquirer = require("inquirer");
+var twitter = require("twitter");
+var spotify = require("node-spotify-api");
+var request = require("request");
+var fs = require("fs");
+
