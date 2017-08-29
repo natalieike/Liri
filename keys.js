80f2dc58f8cba720fa7d806ca7270a690212449c
Natalie Ike <nataliesenger@Natalies-MBP.fios-router.home>
2017-08-29 14:32:46 -0700

    add files for grading purposes

---
+++ keys.js
@@ -0,0 +1,15 @@
+exports.twitterKeys = {
+  consumer_key: '3YcWe54fyOY5qKdqfVZRxrjfm',
+  consumer_secret: '4CN2r51FzCZYZ6JBEXUv0TIkLPmK0bnQfd5Zj1Z4pgli2BXz4J',
+  access_token_key: '901220019035492356-tn3v1Hx3kmiORpUR2k4EhHdPgrCP9QA',
+  access_token_secret: 'OKD9tLCIHoxkXPOjwRhpJyDKEHiOMScCifM74F0pe8MIj',
+};
+
+exports.spotifyKeys = {
+  client_id: '21898ce1e7614016972617324022913b',
+  client_secret: 'da84ef3df78c432dafe3fe5f0573e010',
+};
+
+exports.omdbKeys = {
+	api_key: '40e9cece'
+};
